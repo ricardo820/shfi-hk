5dee628928b243ea06034d0e72af8767ebee0c4b
peto59 <a.krokavec@gmail.com>
2026-04-19 08:59:56 +0200

    fix nav obscure

--- myapp/index.ts
+++ myapp/index.ts
@@ -1,8 +1,18 @@
 import { registerRootComponent } from 'expo';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react';
 
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+function Root() {
+	return React.createElement(
+		SafeAreaProvider,
+		null,
+		React.createElement(App)
+	);
+}
+
+registerRootComponent(Root);
